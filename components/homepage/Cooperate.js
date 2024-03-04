5d2df830f60f72f876d6127ca4f196cf789a0f4a
merve.keles <merve.keles@orgino.com.tr>
2024-03-04 22:30:48 +0300

    fetch local data

--- components/homepage/Cooperate.js
+++ components/homepage/Cooperate.js
@@ -1,10 +1,8 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
 const fetchData = async () => {
     const response = await fetch('https://api.slingacademy.com/v1/sample-data/photos');
     const data = await response.json()
@@ -12,34 +10,45 @@ const fetchData = async () => {
     return data;
 }
 
+const fetchData2 = async () => {
+    const response = await fetch('/cooperate.json');
+    const data = await response.json();
+    console.log(data)
+    return data
+};
+
 export default function Cooperate() {
     const [sliderImg, setSliderImg] = useState();
+    const [myImg, setMyImg] = useState();
     useEffect(() => {
         fetchData().then((response) => setSliderImg(response));
+    }, []);
+    useEffect(() => {
+        fetchData2().then((response) => setMyImg(response))
     }, [])
-    console.log(sliderImg)
     const settings = {
         arrows: true,
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 5,
+        slidesToScroll: 5,
 
     };
     return (
         <div className='slider-container'>
             <Slider {...settings} className='gap-4'>
-                {
+                {/*  {
                     sliderImg.photos.map((i) => {
                         return (
                             <div key={i}><img src={i.url} /></div>
                         )
                     })
+                } */}
+
+                {
+                    myImg.data.map((i) => <div key={i} className='w-[150px]'><img src={i.url} alt={i.name} className='w-full object-fit' /></div>)
                 }
-
-
-
             </Slider>
         </div>
     )
