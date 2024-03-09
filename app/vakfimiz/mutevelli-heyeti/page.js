9dbf5bc86d2a6f5ee57d67377ae97ef2859c5cc0
merve.keles <merve.keles@orgino.com.tr>
2024-03-10 00:51:42 +0300

    yonetim kurulu page and json

--- app/vakfimiz/mutevelli-heyeti/page.js
+++ app/vakfimiz/mutevelli-heyeti/page.js
@@ -1,9 +1,40 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import InsideHero from '@/components/InsideHero';
 
-function page() {
+const fetchData = async () => {
+    const response = await fetch('/data/vakfimiz/mutevelli-heyeti.json');
+    const data = await response.json();
+    return data;
+}
+
+function Page() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetchData().then((res) => setData(res.data));
+    }, []);
+
+    const items = [
+        {
+            name: "Vakfımız",
+            link: "#"
+        },
+        {
+            name: "Mütevelli Heyeti",
+            link: "/vakfimiz/mutevelli-heyeti"
+        }
+    ];
+
     return (
-        <div>Mütevelli Heyeti</div>
+        <div className='min-h-[calc(100vh-130px)]'>
+            <InsideHero items={items} pageTitle="Mütevelli Heyeti" />
+            <div className='custom-container mx-auto bg-white rounded-[10px] pt-20 px-5 md:px-10 pb-[60px] -mt-[50px] box-shadow'>
+                <h1 className='text-title text-[40px] sm:text-[50px] text-center font-semibold'>{data?.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: data?.content }} className='mt-8 text-base md:text-xl text-thirdcolor'></div>
+            </div>
+        </div>
     )
 }
 
-export default page
+export default Page
--- app/vakfimiz/yonetim-kurulu/page.js
+++ app/vakfimiz/yonetim-kurulu/page.js
@@ -1,9 +1,40 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import InsideHero from '@/components/InsideHero';
 
-function page() {
+const fetchData = async () => {
+    const response = await fetch('/data/vakfimiz/yonetim-kurulu.json');
+    const data = await response.json();
+    return data;
+}
+
+function Page() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetchData().then((res) => setData(res.data));
+    }, []);
+
+    const items = [
+        {
+            name: "Vakfımız",
+            link: "#"
+        },
+        {
+            name: "Yönetim Kurulu",
+            link: "/vakfimiz/yonetim-kurulu"
+        }
+    ];
+
     return (
-        <div>Yönetim Kurulu</div>
+        <div className='min-h-[calc(100vh-130px)]'>
+            <InsideHero items={items} pageTitle="Yönetim Kurulu" />
+            <div className='custom-container mx-auto bg-white rounded-[10px] pt-20 px-5 md:px-16 pb-[60px] -mt-[50px] box-shadow'>
+                <h1 className='text-title text-[40px] sm:text-[50px] text-center font-semibold'>{data?.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: data?.content }} className='mt-8 text-base md:text-xl text-thirdcolor'></div>
+            </div>
+        </div>
     )
 }
 
-export default page
+export default Page
