b8976535ea676f5be571027d47e7a33dfc9f6939
merve.keles <merve.keles@orgino.com.tr>
2024-04-07 01:21:30 +0300

    deleted spaces

--- app/iletisim/page.js
+++ app/iletisim/page.js
@@ -48,11 +48,6 @@ function Page() {
                         })
                     }
                 </div>
-
-                <div className='text-center text-base md:text-xl text-thirdcolor'>
-
-                    {/*    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d597.4740428791631!2d-98.9293679209625!3d-81.38757918811322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zODHCsDIzJzE1LjMiUyA5OMKwNTUnMzYuNSJX!5e0!3m2!1str!2str!4v1712438428884!5m2!1str!2str" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
-                </div>
             </div>
 
 
