b7fee3ae38e22a061d8ef969ec38109e5fa102cc
merve.keles <merve.keles@orgino.com.tr>
2024-04-07 17:25:03 +0300

    page title line height

--- components/layout/PageLayout.js
+++ components/layout/PageLayout.js
@@ -9,7 +9,7 @@ function PageLayout({ items, crumbPageTitle, pageTitle, pageData, contentClasses
             <InsideHero items={items} pageTitle={crumbPageTitle} />
             <div className='custom-container mx-auto bg-white rounded-[10px] pt-20 px-5 md:px-16 pb-[60px] -mt-[50px] box-shadow'>
                 <div>
-                    <h1 className='text-title text-3xl sm:text-[40px] md:text-[50px] text-center font-semibold'>{pageTitle}</h1>
+                    <h1 className='text-title text-3xl sm:text-[40px] md:text-[50px] text-center font-semibold leading-[1]'>{pageTitle}</h1>
                     {
                         pageData ? <div dangerouslySetInnerHTML={{ __html: pageData }} className={`mt-8 text-base md:text-xl text-thirdcolor ${contentClasses}`}></div> : null
                     }
