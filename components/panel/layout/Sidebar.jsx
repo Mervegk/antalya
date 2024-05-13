0d24276bb3b524c1dff609a2bf4b217c8272bc91
merve.keles <merve.keles@orgino.com.tr>
2024-05-13 22:53:28 +0300

    sidebar user section styling

--- components/panel/layout/Sidebar.jsx
+++ components/panel/layout/Sidebar.jsx
@@ -1,9 +1,8 @@
 "use client"
 import React, { createContext, useContext, useState } from 'react';
 import Image from 'next/image';
-import { RiHome4Line, RiArrowLeftDoubleLine, RiGraduationCapLine, RiSchoolLine, RiMegaphoneLine, RiGalleryLine, RiPhoneLine, RiOpenArmLine } from "react-icons/ri";
+import { RiHome4Line, RiArrowLeftDoubleLine, RiGraduationCapLine, RiSchoolLine, RiMegaphoneLine, RiGalleryLine, RiPhoneLine, RiOpenArmLine, RiMore2Line } from "react-icons/ri";
 import { LogoImage } from '@/app/data/images';
-import { usePathname } from 'next/navigation';
 
 const SidebarContext = createContext();
 
@@ -17,7 +16,7 @@ export default function Sidebar({ children }) {
             <img src={LogoImage.image.src} className={`overflow-hidden transition-all duration-300 $ `} alt='logo' />
           </div>
           <button onClick={() => setCollapsed((prev) => !prev)}
-            className='p-1 bg-thirdcolor/5 rounded-lg text-3xl text-secondcolor hover:bg-thirdcolor/15 transition-all duration-100 ease-linear'><RiArrowLeftDoubleLine className={`transition-all ${collapsed ? 'rotate-180' : 'rotate-0'}`} />
+            className='p-1 bg-thirdcolor/5 rounded-lg text-3xl text-secondcolor hover:bg-thirdcolor/15 transition-all duration-300 ease-linear'><RiArrowLeftDoubleLine className={`transition-all ${collapsed ? 'rotate-180' : 'rotate-0'}`} />
           </button>
         </div>
         <SidebarContext.Provider value={{ collapsed }}>
@@ -30,15 +29,26 @@ export default function Sidebar({ children }) {
             <SidebarItem icon={<RiPhoneLine />} text='İletişim' link='/yonetim-paneli/iletisim' alert='İletişim' />
             <SidebarItem icon={<RiMegaphoneLine />} text='Duyurular' alert='Duyurular' />
           </ul>
-          <div className={`border-t flex p-2 ${!collapsed ? 'justify-center' : ''}`} >
-            <div className='min-w-12 min-h-12 w-12 h-12 rounded-md'><img src='https://placehold.co/600x400' className='h-full w-full object-cover' /></div>
-            <div className={`flex items-center w-52 ml-3 transition-all duration-200 ease-linear ${!collapsed ? 'hidden' : ''}`}>
-              <div>
-                <h4 className='font-semibold'>Ayşe Fatma</h4>
-              </div>
-            </div>
-          </div>
         </SidebarContext.Provider>
+        <div className={`border-t flex p-3 ${!collapsed ? 'mx-auto' : ''}`}>
+          <img
+            src="https://placehold.co/600x400"
+            alt=""
+            className="w-10 h-10 rounded-md"
+          />
+          <div
+            className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${collapsed ? "w-52 ml-3" : "w-0"}
+          `}
+          >
+            <div className="leading-4">
+              <h4 className="font-semibold">Ayşe Fatma</h4>
+              <span className="text-xs text-gray-600">johndoe@gmail.com</span>
+            </div>
+            <RiMore2Line size={20} />
+          </div>
+        </div>
       </nav>
     </aside>
   )
@@ -47,7 +57,7 @@ export default function Sidebar({ children }) {
 export function SidebarItem({ icon, text, alert, link }) {
   const { collapsed } = useContext(SidebarContext);
   return (
-    <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-all duration-150 ease-linear ${collapsed ? '' : 'justify-center group'} ${window.location.pathname == link ? 'sidebar-menu-active' : 'sidebar-menu'} `}
+    <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-all duration-300 ease-linear ${collapsed ? '' : 'justify-center group'} ${window.location.pathname == link ? 'sidebar-menu-active' : 'sidebar-menu'} `}
       onClick={() => { window.location.pathname = link }}
     > <i className='text-2xl'>{icon}</i> <span className={`overflow-hidden transition-all ${collapsed ? 'w-52 ml-3' : 'w-0'} `}>{text}</span>
       {!collapsed && <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-[#f8fbfe] text-secondcolor text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>{text}</div>}
